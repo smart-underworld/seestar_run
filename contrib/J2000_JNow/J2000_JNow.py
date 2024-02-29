b9df76dd6f929efeeb1cbefde120b1bc1666c569
Jari Backman <63860756+jabamula@users.noreply.github.com>
2024-02-29 15:40:40 +0200

    Add files via upload
    Updating python file to version 1.0.1

--- contrib/J2000_JNow/J2000_JNow.py
+++ contrib/J2000_JNow/J2000_JNow.py
@@ -1,3 +1,4 @@
+# Conversion 4
 from astropy.coordinates import SkyCoord, FK5, FK4, Angle, ICRS
 from astropy.time import Time
 import astropy.units as u
@@ -8,55 +9,80 @@ import tkinter as tk
 # This program performs coordinate transformations.
 # The option is to transform epoch J2000 to epoch on date (JNow) and vice versa
 # The window shows both decimal (1.25, 40.5) values as well the HMSDMS (1:15:00 40:30:00) values for both epochs
-# Especially useful when the telescope uses JNow and reference data J2000
-# 20240221 Jari Backman, 'jabamula', jari@sinijari.fi
+# Especially useful when the telescope use needs JNow and the reference data is J2000
+# Jari Backman, 'jabamula', jari@sinijari.fi
+#
+# version 1.0.1
+# changes:
+# code stylising
+# check the format of the input coordinates
+# HMSDMS results:seconds kept without decimals. The accuracy is not needed 
+# added version label
+# added developer label
+
+version = "1.0.1"
+
+def show_error_popup(invalid_input):
+    error_window = tk.Toplevel(root)
+    error_window.title("Input Error")
+    message = f"Invalid Input: \"{invalid_input}\".\nPlease enter coordinates in Decimal (1.23, 2.34) or HMSDMS (1:14:50.3 2:34:12.3) format."
+    tk.Label(error_window, text=message).pack(pady=10, padx=10)
+    tk.Button(error_window, text="OK", command=error_window.destroy).pack(pady=5)
+
+def validate_inputs(coords_list):
+    # Regex patterns for decimal and HMSDMS formats
+    decimal_pattern = re.compile(r'^-?\d+(\.\d+)?, -?\d+(\.\d+)?$')
+    hmsdms_pattern = re.compile(r'^-?\d+:\d+:\d+(\.\d+)?\s+-?\d+:\d+:\d+(\.\d+)?$')
+    
+    for coord in coords_list:
+        if coord:  # Check only if the input field is not empty
+            if not (decimal_pattern.match(coord) or hmsdms_pattern.match(coord)):
+                show_error_popup(coord)
+                return False
+    return True
+
 
 def convert_coordinates():
     # Get the input values from the GUI
-    #dec_input = dec_entry.get()
-    #hmsdms_input = hmsdms_entry.get()
     j2000_decimal = input_entries[0].get()
     j2000_hmsdms = input_entries[1].get()
     jnow_decimal = input_entries[2].get()
     jnow_hmsdms = input_entries[3].get()
+    
+    # Validate all inputs
+    inputs = [j2000_decimal, j2000_hmsdms, jnow_decimal, jnow_hmsdms]
+    if not validate_inputs(inputs):
+        return  # Exit the function if any input is invalid
 
     # today
     tm = datetime.now()
     
     # decimal J2000
-    if j2000_decimal != "": 
-        J2k = 1
-        ra, dec = map(float, j2000_decimal.strip('()').split(','))
-        coin = ICRS(ra*u.hour, dec*u.degree)
-        inCoords = str(coin.ra/15) + ' ' + str(coin.dec)
-        inCoords = inCoords.replace('d', ':').replace('m',':').replace('s', '')
-        j2000 = SkyCoord(ra, dec, unit=(u.hourangle, u.deg))
-    # hmsdms J2000    
-    elif j2000_hmsdms != "":
-        J2k = 1
-        inCoords = j2000_hmsdms
-        J2000coords = [inCoords, inCoords.replace(":"," ")]
-        c = SkyCoord(J2000coords, frame='icrs', unit=(u.hourangle, u.deg))
-        ra = c.ra.hour[0]
-        dec = c.dec.deg[0]
-        j2000 = SkyCoord(ra, dec, unit=(u.hourangle, u.deg))
-    # decimal JNow
-    elif jnow_decimal != "":
-        J2k = 0
-        ra, dec = map(float, jnow_decimal.strip('()').split(','))
-        coin = ICRS(ra*u.hour, dec*u.degree)
-        inCoords = str(coin.ra/15) + ' ' + str(coin.dec)
-        inCoords = inCoords.replace('d', ':').replace('m',':').replace('s', '')
-        jNow = SkyCoord(ra, dec, unit=(u.hourangle, u.deg), frame=FK5(equinox=tm))
-    # hmsdms JNow    
-    else:
-        J2k = 0
-        inCoords = jnow_hmsdms
-        jNow = [inCoords, inCoords.replace(":"," ")]
-        c = SkyCoord(jNow, frame='icrs', unit=(u.hourangle, u.deg))
-        ra = c.ra.hour[0]
-        dec = c.dec.deg[0]
-        jNow = SkyCoord(ra, dec, unit=(u.hourangle, u.deg), frame=FK5(equinox=tm))      
+    # J2000 coordinates
+    if j2000_decimal != "" or j2000_hmsdms != "":
+        J2k = 1    
+        if j2000_decimal != "":
+            radi, decdi = map(float, j2000_decimal.strip('()').split(','))
+        elif j2000_hmsdms != "":
+            inCoords = j2000_hmsdms
+            c = SkyCoord([inCoords, inCoords.replace(":", " ")], frame='icrs', unit=(u.hourangle, u.deg))
+            radi = c.ra.hour[0]
+            decdi = c.dec.deg[0]
+
+        j2000 = SkyCoord(radi, decdi, unit=(u.hourangle, u.deg))
+
+    # JNow coordinates
+    elif jnow_decimal != "" or jnow_hmsdms != "":
+        J2k = 0    
+        if jnow_decimal != "":
+            rani, decni = map(float, jnow_decimal.strip('()').split(','))
+        elif jnow_hmsdms != "":
+            inCoords = jnow_hmsdms
+            c = SkyCoord([inCoords, inCoords.replace(":", " ")], frame='icrs', unit=(u.hourangle, u.deg))
+            rani = c.ra.hour[0]
+            decni = c.dec.deg[0]
+
+        jNow = SkyCoord(rani, decni, unit=(u.hourangle, u.deg), frame=FK5(equinox=tm))
 
     # The logic (Conversion)
     # J2000 to Jnow
@@ -66,56 +92,58 @@ def convert_coordinates():
     else:
         j2000 = jNow.transform_to(FK5(equinox="J2000"))
           
-    # J2000 output
+    rado = j2000.ra.hour
+    decdo = j2000.dec.deg  
+    rano = jNow.ra.hour
+    decno = jNow.dec.deg  
+
+            # J2000 output
     if J2k == 1:
-        j2000_dec = str("%0.4f" % ra) + ", " + str("%0.4f" % dec)
-        j2000_hmsdms = inCoords
+        ra = radi
+        dec = decdi
+        j2000_dec = f"{ra:.4f}, {dec:.4f}"
+        j2000_hmsdms = f"{j2000.ra.hms.h:02.0f}:{j2000.ra.hms.m:02.0f}:{j2000.ra.hms.s:02.0f} {j2000.dec.dms.d:02.0f}:{abs(j2000.dec.dms.m):02.0f}:{abs(j2000.dec.dms.s):02.0f}"
     else:
-        outCoords = j2000.to_string('hmsdms').replace("h", ":").replace("d", ":").replace("m", ":").replace("s", "")
-        outCoords = f"{j2000.ra.hms.h:02.0f}{':'}{j2000.ra.hms.m:02.0f}{':'}{j2000.ra.hms.s:05.2f} {j2000.dec.dms.d:02.0f}{':'}{abs(j2000.dec.dms.m):02.0f}{':'}{abs(j2000.dec.dms.s):05.2f}"
-        j2000_hmsdms = outCoords 
-        ra_N = j2000.ra.hour
-        dec_N = j2000.dec.deg  
-        j2000_dec =  str("%0.4f" % ra_N) + ", " + str("%0.4f" % dec_N)
+        ra = rado
+        dec = decdo
+        j2000_dec = f"{ra:.4f}, {dec:.4f}"
+        j2000_hmsdms = f"{j2000.ra.hms.h:02.0f}:{j2000.ra.hms.m:02.0f}:{j2000.ra.hms.s:02.0f} {j2000.dec.dms.d:02.0f}:{abs(j2000.dec.dms.m):02.0f}:{abs(j2000.dec.dms.s):02.0f}"
 
     # JNow output
     if J2k == 1:
+        ra = rano
+        dec = decno
         outCoords = jNow.to_string('hmsdms').replace("h", ":").replace("d", ":").replace("m", ":").replace("s", "")
-        outCoords = f"{jNow.ra.hms.h:02.0f}{':'}{jNow.ra.hms.m:02.0f}{':'}{jNow.ra.hms.s:05.2f} {jNow.dec.dms.d:02.0f}{':'}{abs(jNow.dec.dms.m):02.0f}{':'}{abs(jNow.dec.dms.s):05.2f}"
+        outCoords = f"{jNow.ra.hms.h:02.0f}:{jNow.ra.hms.m:02.0f}:{jNow.ra.hms.s:02.0f} {jNow.dec.dms.d:02.0f}:{abs(jNow.dec.dms.m):02.0f}:{abs(jNow.dec.dms.s):02.0f}"
         jnow_hmsdms = outCoords 
-        ra_N = jNow.ra.hour
-        dec_N = jNow.dec.deg  
-        jnow_dec =  str("%0.4f" % ra_N) + ", " + str("%0.4f" % dec_N)
+        jnow_dec = f"{ra:.4f}, {dec:.4f}"
     else:
-        jnow_dec = str("%0.4f" % ra) + ", " + str("%0.4f" % dec)
-        jnow_hmsdms = inCoords
- 
+        ra = rani
+        dec = decni
+        jnow_dec = f"{ra:.4f}, {dec:.4f}"
+        jnow_hmsdms = f"{jNow.ra.hms.h:02.0f}:{jNow.ra.hms.m:02.0f}:{jNow.ra.hms.s:02.0f} {jNow.dec.dms.d:02.0f}:{abs(jNow.dec.dms.m):02.0f}:{abs(jNow.dec.dms.s):02.0f}"
+
     # Update the output labels with the results
     outputs["J2000 Decimal"].config(text=j2000_dec)
     outputs["J2000 HMSDMS"].config(text=j2000_hmsdms)
     outputs["JNow Decimal"].config(text=jnow_dec)
     outputs["JNow HMSDMS"].config(text=jnow_hmsdms)
-
+    
 # Create the main window
-root = tk.Tk()
-root.title("Coordinate Converter J2000 vs JNow")
-
 def disable_other_entries(event, entry_set):
     for entry in entry_set:
         if entry.get() and entry is not event.widget:
             entry.configure(state='disabled')
 
 def reset_fields():
-    # Clear all entries
     for entry in input_entries:
         entry.delete(0, tk.END)
-        entry.configure(state='normal')  # Re-enable the entry
+        entry.configure(state='normal')
 
-    # Clear all outputs
-    for label in outputs.values():
-        label.config(text="N/A")
+# Initialize the main window
+root = tk.Tk()
+root.title("Coordinate Converter J2000 vs JNow")
 
-# Define the padding
 pad_x = 10
 pad_y = 5
 
@@ -128,7 +156,7 @@ labels = ["J2000 Decimal", "J2000 HMSDMS", "JNow Decimal", "JNow HMSDMS"]
 for i, label_text in enumerate(labels):
     tk.Label(input_frame, text=label_text).grid(row=0, column=i, padx=pad_x, pady=pad_y)
 
-# Define and place input entry fields
+# Input entry fields
 input_entries = []
 for i in range(len(labels)):
     entry = tk.Entry(input_frame)
@@ -142,6 +170,10 @@ convert_button.grid(row=1, column=len(labels), padx=pad_x, pady=pad_y, sticky="e
 reset_button = tk.Button(input_frame, text="Reset", command=reset_fields)
 reset_button.grid(row=1, column=len(labels)+1, padx=pad_x, pady=pad_y, sticky="ew")
 
+# Add version label above the buttons
+version_label = tk.Label(input_frame, text="Version 1.0.1")
+version_label.grid(row=2, columnspan=len(labels) + 2, pady=pad_y)
+
 # Create the output frame
 output_frame = tk.Frame(root)
 output_frame.pack(padx=pad_x, pady=pad_y, fill="x")
@@ -154,6 +186,10 @@ for i, label_text in enumerate(output_labels):
     output = tk.Label(output_frame, text="N/A", relief="sunken")
     output.grid(row=i, column=1, padx=pad_x, pady=pad_y, sticky="ew")
     outputs[label_text] = output
+
+# Developer info at the bottom right corner
+developer_label = tk.Label(root, text="jabamula/2024", anchor="e")
+developer_label.pack(side="bottom", fill="x")
 
 # Make the columns in all grids resizable
 for i in range(len(labels)):
